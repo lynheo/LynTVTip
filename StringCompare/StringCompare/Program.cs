e9690cfe9bb912b0683f9f992ee928e0a2f3411e
lynheo <siroitsuki@gmail.com>
2019-04-23 21:51:06 +0900

    C# String

---
+++ StringCompare/StringCompare/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Text;
+
+namespace StringCompare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Foo0();
+            Foo1();
+            Foo2();
+            Foo3();
+            Foo4();
+        }
+
+        static void Foo0()
+        {
+            string str1 = "Lyn";
+            string str2 = "Lyn";
+
+            Console.WriteLine(str1 == str2);
+        }
+
+        static void Foo1()
+        {
+            object str = "Lyn";
+            Console.WriteLine(str == "Lyn");
+        }
+
+        static void Foo2()
+        {
+            object str1 = "Lyn";
+            var str2 = (new StringBuilder().Append('L').Append('y').Append('n')).ToString();
+            Console.WriteLine(str1 == str2);
+        }
+
+        static void Foo3()
+        {
+            object str1 = "Lyn";
+            object str2 = "Lyn";
+            object str3 = "Lyn";
+
+            Console.WriteLine(str1 == str2);
+        }
+
+        static void Foo4()
+        {
+            string str1 = "Lyn";
+            var str2 = (new StringBuilder().Append('L').Append('y').Append('n')).ToString();
+            Console.WriteLine(str1 == str2);
+        }
+    }
+}
